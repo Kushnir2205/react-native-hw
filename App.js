d27b1c5da60c119ab67a6c70e819e202dfdf82d0
Kushnir2205 <kushnirroman22@gmail.com>
2023-10-18 21:58:33 +0300

    add main stack

--- App.js
+++ App.js
@@ -1,17 +1,74 @@
+// import { StatusBar } from "expo-status-bar";
+// import { StyleSheet, View } from "react-native";
+// import "react-native-gesture-handler";
+// import { NavigationContainer } from "@react-navigation/native";
+// import { createStackNavigator } from "@react-navigation/stack";
+
+// import RegistrationScreen from "./Screens/AuthScreens/RegistrationScreen";
+// import LoginScreen from "./Screens/AuthScreens/LoginScreen";
+// import Home from "./Screens/HomePages/Home";
+
+// const MainStack = createStackNavigator();
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <NavigationContainer>
+//         <MainStack.Navigator
+//           initialRouteName="Login"
+//           screenOptions={{ headerShown: false }}
+//         >
+//           <MainStack.Screen
+//             name="Registration"
+//             component={RegistrationScreen}
+//           />
+//           <MainStack.Screen name="Login" component={LoginScreen} />
+//           <MainStack.Screen name="Home" component={Home} />
+//         </MainStack.Navigator>
+//         {/* <StatusBar style="auto" /> */}
+//       </NavigationContainer>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//   },
+// });
+
+import React from "react";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 
+import LoginScreen from "./Screens/AuthScreens/LoginScreen";
 import RegistrationScreen from "./Screens/AuthScreens/RegistrationScreen";
+import Home from "./Screens/HomePages/Home";
 
-// import LoginScreen from "./Screens/AuthScreens/LoginScreen";
+const MainStack = createStackNavigator();
 
 export default function App() {
   return (
     <View style={styles.container}>
-      {/* <LoginScreen /> */}
-      <RegistrationScreen />
-
-      <StatusBar style="auto" />
+      <NavigationContainer>
+        <MainStack.Navigator
+          initialRouteName="Login"
+          screenOptions={{ headerShown: false }}
+        >
+          <MainStack.Screen
+            name="Registration"
+            component={RegistrationScreen}
+          />
+          <MainStack.Screen name="Login" component={LoginScreen} />
+          <MainStack.Screen name="Home" component={Home} />
+        </MainStack.Navigator>
+        {/* <StatusBar style="auto" /> */}
+      </NavigationContainer>
     </View>
   );
 }
@@ -20,7 +77,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
